5a87140a1d4473f30bca837cffcfe264a76d657d
Teguh Purwo Jati Kusumo <sembuarang@yahoo.com>
2024-04-18 08:31:54 +0700

    update resources/views/daftarhadir/config.blade.php

--- resources/views/daftarhadir/config.blade.php
+++ resources/views/daftarhadir/config.blade.php
@@ -12,7 +12,7 @@
 @section('admin-container')
     <section class="section">
         <div class="section-header">
-            <h1>Advanced Forms</h1>
+            <h1>Advanced Forms nih boss</h1>
             <div class="section-header-breadcrumb">
                 <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
                 <div class="breadcrumb-item"><a href="#">Forms</a></div>
