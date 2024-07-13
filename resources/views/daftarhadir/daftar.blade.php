b15b857e8c167d118a748aa0fdbef2e4cdf9ca4f
synysmike <sembuarang@yahoo.com>
2024-07-13 12:08:25 +0700

    test export datatable list dh

--- resources/views/daftarhadir/daftar.blade.php
+++ resources/views/daftarhadir/daftar.blade.php
@@ -71,7 +71,7 @@
                 columns.push(my_item);
             });
             //datatable yajra
-            var table1 = $('#table-1').dataTable({
+            var dt = $('#table-1').dataTable({
                 processing: true,
                 serverSide: true, //aktifkan server-side 
                 ajax: {
@@ -86,33 +86,62 @@
                 ],
                 iDisplayLength: 10,
                 dom: 'Bfrtip',
-                buttons: [
-                    'copy', 'excel', 'pdf', 'csv'
-                ]
-            });
-            new $.fn.dataTable.Buttons(table1, {
-<<<<<<< HEAD
                 buttons: [{
                     "extend": 'excelHtml5',
-                    "text": 'Eksport Excel',
-                    messageTop: 'Rekap Daftar Hadir Pelatihan Asesor 2024',
+                    "text": 'Ekspor Excel',
+                    "messageTop": 'Rekap Daftar Hadir Pelatihan Asesor 2024',
                     "action": newexportaction
                 }],
-=======
-                buttons: [
-                    'copy', 'excel', 'pdf'
-                ],
-                exportOptions: {
-                    modifer: {
-                        page: 'all',
-                        search: 'none'
-                    }
-                }
->>>>>>> parent of f8bf9141 (fix export data list-dh)
             });
-            table1.buttons(0, null).container().prependTo(
-                table1.table().container()
-            );
+
+            function newexportaction(e, dt , button, config) {
+                var self = this;
+                var oldStart = dt.settings()[0]._iDisplayStart;
+                dt .one('preXhr', function(e, s, data) {
+                    // Just this once, load all data from the server...
+                    data.start = 0;
+                    data.length = 2147483647;
+                    dt.one('preDraw', function(e, settings) {
+                        // Call the original action function
+                        if (button[0].className.indexOf('buttons-copy') >= 0) {
+                            $.fn.dataTable.ext.buttons.copyHtml5.action.call(self, e, dt, button,
+                                config);
+                        } else if (button[0].className.indexOf('buttons-excel') >= 0) {
+                            $.fn.dataTable.ext.buttons.excelHtml5.available(dt, config) ?
+                                $.fn.dataTable.ext.buttons.excelHtml5.action.call(self, e, dt,
+                                    button, config) :
+                                $.fn.dataTable.ext.buttons.excelFlash.action.call(self, e, dt,
+                                    button, config);
+                        } else if (button[0].className.indexOf('buttons-csv') >= 0) {
+                            $.fn.dataTable.ext.buttons.csvHtml5.available(dt, config) ?
+                                $.fn.dataTable.ext.buttons.csvHtml5.action.call(self, e, dt, button,
+                                    config) :
+                                $.fn.dataTable.ext.buttons.csvFlash.action.call(self, e, dt, button,
+                                    config);
+                        } else if (button[0].className.indexOf('buttons-pdf') >= 0) {
+                            $.fn.dataTable.ext.buttons.pdfHtml5.available(dt, config) ?
+                                $.fn.dataTable.ext.buttons.pdfHtml5.action.call(self, e, dt, button,
+                                    config) :
+                                $.fn.dataTable.ext.buttons.pdfFlash.action.call(self, e, dt, button,
+                                    config);
+                        } else if (button[0].className.indexOf('buttons-print') >= 0) {
+                            $.fn.dataTable.ext.buttons.print.action(e, dt, button, config);
+                        }
+                        dt.one('preXhr', function(e, s, data) {
+                            // DataTables thinks the first item displayed is index 0, but we're not drawing that.
+                            // Set the property to what it was before exporting.
+                            settings._iDisplayStart = oldStart;
+                            data.start = oldStart;
+                        });
+                        // Reload the grid with the original page. Otherwise, API functions like table.cell(this) don't work properly.
+                        setTimeout(dt.ajax.reload, 0);
+                        // Prevent rendering of the full data to the DOM
+                        return false;
+                    });
+                });
+                // Requery the server with the new one-time export settings
+                dt.ajax.reload();
+            };
         });
     </script>
 @endpush
