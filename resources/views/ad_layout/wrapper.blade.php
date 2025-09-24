6feeba7d6bba680c2d2d10c0d8158a26cf8c0933
synysmike <sembuarang@yahoo.com>
2025-09-24 13:18:31 +0000

    Refactor asset links in wrapper view to use the asset() helper for improved path management

--- resources/views/ad_layout/wrapper.blade.php
+++ resources/views/ad_layout/wrapper.blade.php
@@ -8,7 +8,7 @@
     <title>{{ $tittle }} - BAN-PDM Jatim</title>
 
     <!-- General CSS Files -->
-    <link rel="stylesheet" href="/admin_theme/library/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset('admin_theme/library/bootstrap/dist/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
         integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -16,10 +16,10 @@
     <!-- CSS Libraries -->
     @stack('css-custom')
     <!-- CSS Libraries -->
-    <link rel="stylesheet" href="/admin_theme/library/prismjs/themes/prism.min.css">
+    <link rel="stylesheet" href="{{ asset('admin_theme/library/prismjs/themes/prism.min.css') }}">
     <!-- Template CSS -->
-    <link rel="stylesheet" href="/admin_theme/css/style.css">
-    <link rel="stylesheet" href="/admin_theme/css/components.css">
+    <link rel="stylesheet" href="{{ asset('admin_theme/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin_theme/css/components.css') }}">
 
     <!-- Start GA -->
     {{-- <script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script> --}}
@@ -44,7 +44,7 @@
             @include('ad_layout.side') --}}
             <!-- Content -->
             <div class="main-content">
-                
+
                 @yield('admin-container')
             </div>
             <!-- Footer -->
@@ -54,7 +54,7 @@
                         BAN-PDM Provinsi Jawa Timur </a>
                 </div>
                 <div class="footer-right">
-                    ir.teguh IT BANPDMJATIM 
+                    ir.teguh IT BANPDMJATIM
                 </div>
             </footer>
         </div>
@@ -62,25 +62,25 @@
 
     <!-- General JS Scripts -->
     <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
-    <script src="/admin_theme/library/popper.js/dist/umd/popper.js"></script>
-    <script src="/admin_theme/library/tooltip.js/dist/umd/tooltip.js"></script>
-    <script src="/admin_theme/library/bootstrap/dist/js/bootstrap.min.js"></script>
-    <script src="/admin_theme/library/jquery.nicescroll/dist/jquery.nicescroll.min.js"></script>
-    <script src="/admin_theme/library/moment/min/moment.min.js"></script>
-    <script src="/admin_theme/js/stisla.js"></script>
+    <script src="{{ asset('admin_theme/library/popper.js/dist/umd/popper.js') }}"></script>
+    <script src="{{ asset('admin_theme/library/tooltip.js/dist/umd/tooltip.js') }}"></script>
+    <script src="{{ asset('admin_theme/library/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('admin_theme/library/jquery.nicescroll/dist/jquery.nicescroll.min.js') }}"></script>
+    <script src="{{ asset('admin_theme/library/moment/min/moment.min.js') }}"></script>
+    <script src="{{ asset('admin_theme/js/stisla.js') }}"></script>
 
     <!-- JS Libraies -->
-    {{-- <script src="/admin_theme/library/bootstrap-daterangepicker/daterangepicker.js"></script>
-    <script src="/admin_theme/library/summernote/dist/summernote-bs4.js"></script> --}}
+    {{-- <script src="{{ asset('admin_theme/library/bootstrap-daterangepicker/daterangepicker.js') }}"></script>
+    <script src="{{ asset('admin_theme/library/summernote/dist/summernote-bs4.js') }}"></script> --}}
     <!-- JS Libraies -->
-    <script src="/admin_theme/library/prismjs/prism.js"></script>
+    <script src="{{ asset('admin_theme/library/prismjs/prism.js') }}"></script>
 
 
     <!-- Page Specific JS File -->
 
     <!-- Template JS File -->
-    <script src="/admin_theme/js/scripts.js"></script>
-    <script src="/admin_theme/js/custom.js"></script>
+    <script src="{{ asset('admin_theme/js/scripts.js') }}"></script>
+    <script src="{{ asset('admin_theme/js/custom.js') }}"></script>
     @stack('js-custom')
 </body>
 
